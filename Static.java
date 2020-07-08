7082c023e2b67149cf6759ed77e64da1aa07ccc9
Alekha-32 <65142206+Alekha-32@users.noreply.github.com>
2020-07-08 17:26:20 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,32 @@
+package logicalOperators;
+
+public class Static {
+	
+		   boolean bool;
+		   byte by;
+		   char ch;
+		  double d;
+		   float f;
+		  int i = 50000;
+		  long l;
+		 short sh;
+		   String str;
+
+		  
+		  
+		  
+		  public static void main(String[] args) {
+    Static a = new Static();
+		    System.out.println("Bool :" + a.bool);
+		    System.out.println("Byte :" + a. by);
+		    System.out.println("Character:" + a.ch);
+		    System.out.println("Double :" + a.d);
+		    System.out.println("Float :" + a.f);
+		    System.out.println("Integer :" +a. i);
+		    System.out.println("Long :" + a.l);
+		    System.out.println("Short :" + a.sh);
+		    System.out.println("String :" + a.str);
+		  }
+		}
+
+
